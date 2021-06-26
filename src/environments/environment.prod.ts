1296061965be432f0b5f47ff89016edcdb5d6849
epepftw <ipinggxlirangg@gmail.com>
2021-06-27 02:22:46 +0800

    Prod Auth Changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  authenticate: 'http://localhost:3000/api/users/authenticate',
+  authenticate: 'http://192.168.100.11:3000/api/users/authenticate',
   api: 'https://jsonplaceholder.typicode.com',
   mediaFiles: '/photos?_start=0&_limit=40',
   users: '/users'
