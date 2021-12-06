29f64d3869710fb2aadd033fc916146204e7ff58
Epep <ipinggxlirangg@gmail.com>
2021-12-07 05:48:05 +0800

    FILESTACK

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,7 @@ import { endpoints } from "./endpoints";
 export const environment = {
   production: false,
   base_api: 'https://rcdt-api.herokuapp.com/api',
-  filestackAPI: //'AqJu2MTmS4OrNOAI83ApXz',
-                'A7dL8mkNqRwmsg0gvBji6z',
+  filestackAPI: 'A46sEWzSWRjyg8LO7w6ntz',
 
   get: endpoints.get,
   post: endpoints.post,
